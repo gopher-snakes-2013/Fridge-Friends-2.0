be3197376722e7c5fde444a0d40bd1fde47f5109
danbender <anniesing1014@gmail.com>
2013-11-09 00:51:46 -0800

    add feature test: user that is not creator cannot delete fridge they didn't create

--- spec/features/creator_can_delete_fridge.rb
+++ spec/features/creator_can_delete_fridge.rb
@@ -7,6 +7,8 @@ feature "User can delete a fridge" do
 
   let!(:user) { User.new(name: 'Carter1', email: 'carter11@example.com', password: 'password', phone_number: '407-774-9393', customer_id: '123456788') }
 
+  let!(:user_2) { User.new(name: 'Bob', email: 'bob@example.com', password: 'bobpassword', phone_number: '234-533-4354', customer_id: '123456788') }
+
   scenario "when they are the original fridge creator" do
     signup_user(user)
     fill_in "fridge_name", with: "Home Fridge"
@@ -15,4 +17,23 @@ feature "User can delete a fridge" do
     click_on "Delete Fridge"
     expect(page).to_not have_content "Home Fridge"
   end
+
+  scenario "but not when they are not the original fridge creator" do
+    signup_user(user)
+    click_link "logout"
+    signup_user(user_2)
+    fill_in "fridge_name", with: "Bob's Home Fridge"
+    click_on "Add Fridge"
+    expect(page).to have_content "Bob's Home Fridge"
+    click_link "Bob's Home Fridge"
+    fill_in 'user_email', with: 'user.email'
+    click_on 'Sign up'
+    click_link "logout"
+    fill_in 'session_email', with: user.email
+    fill_in 'session_password', with: user.password
+    click_on "Sign in"
+    expect(page).to_not have_content "Delete Fridge"
+  end
 end
+
+

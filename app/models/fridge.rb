c4d1f513943bf0ecc0b3710a681f6930d24dad53
carter <carter.sowers@gmail.com>
2013-11-09 13:32:58 -0800

    fix tests, add attr accessible to fridge model for creator_id

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,5 +1,5 @@
 class Fridge < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :creator_id
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => [:creator_id]
   has_and_belongs_to_many :users

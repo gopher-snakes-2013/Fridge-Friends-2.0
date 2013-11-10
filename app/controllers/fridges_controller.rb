bfa6444b6066ba7bf65caac6013ee36bebb0e476
danbender <anniesing1014@gmail.com>
2013-11-09 22:37:44 -0800

    add feature:show friends of fridges in side bar

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -23,6 +23,7 @@ class FridgesController < ApplicationController
     @item = Item.new
     @items = @fridge.items.all
     @find_user_email = User.new
+    @friends = find_only_friends_of_fridge(current_user, @fridge)
   end
 
   def destroy

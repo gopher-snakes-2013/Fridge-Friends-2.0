a3f81c0f4f06c91715368e3e6344e3753dc6c181
danbender <anniesing1014@gmail.com>
2013-11-09 20:59:27 -0800

    add feature: fridge friends recieve text messages whenever a new item is added to a fridge

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
-# require 'rubygems'
+require 'rubygems'
 require 'twilio-ruby'
+include ApplicationHelper
 
-#figure out how to put this in an env file
 class ItemsController < ApplicationController
   def index
     @item = Item.new
@@ -10,19 +10,25 @@ class ItemsController < ApplicationController
   end
 
   def create
-  account_sid = ENV['ACCOUNT_SID']
-  auth_token = ENV['AUTH_TOKEN']
-  @client = Twilio::REST::Client.new account_sid, auth_token
+    account_sid = ENV['ACCOUNT_SID']
+    auth_token = ENV['AUTH_TOKEN']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    current_user_number = convert_phone_number(current_user.phone_number)
+    current_user_array = [current_user]
+
     new_item = Item.new(params[:item])
     new_item.fridge_id = params[:fridge_id]
+    current_fridge = Fridge.find(params[:fridge_id])
+    fridge_friends = current_fridge.users - current_user_array
+
     if new_item.save
-      message = @client.account.sms.messages.create(body: "you just created a new item called #{new_item.name}",
-                                              to: ENV['ANNIES_NUMBER'],
-                                              from: '14153602257')
-      puts message.sid
-
-
-
+      @client.account.sms.messages.create(body: "you just created a new item in #{current_fridge.name} called #{new_item.name}", to: current_user_number, from: '14153602257')
+      puts "Message sent to item creator: #{current_user.name}"
+      fridge_friends.each do |friend|
+        friend_phone = convert_phone_number(friend.phone_number)
+        @client.account.sms.messages.create(body: "#{current_user.name} just created a new item in #{current_fridge.name} called #{new_item.name}", to: friend_phone, from: '14153602257')
+        puts "Message sent to fridge friend: #{friend.name}"
+      end
       redirect_to fridge_path(new_item.fridge.id)
     else
       flash[:add_item_notice] = new_item.errors.full_messages.join(", ")
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def convert_phone_number(number)
+    number = "1#{number.gsub(/-/, '')}"
+  end
 end

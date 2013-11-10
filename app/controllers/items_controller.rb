81a45101df5645bd46aa365ad0a22beee143da0d
danbender <anniesing1014@gmail.com>
2013-11-09 21:36:09 -0800

    refactor sending text logic into helper methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,29 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    account_sid = ENV['ACCOUNT_SID']
-    auth_token = ENV['AUTH_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    current_user_number = convert_phone_number(current_user.phone_number)
-    current_user_array = [current_user]
-
-    new_item = Item.new(params[:item])
-    new_item.fridge_id = params[:fridge_id]
-    current_fridge = Fridge.find(params[:fridge_id])
-    fridge_friends = current_fridge.users - current_user_array
-
-    if new_item.save
-      @client.account.sms.messages.create(body: "you just created a new item in #{current_fridge.name} called #{new_item.name}", to: current_user_number, from: '14153602257')
-      puts "Message sent to item creator: #{current_user.name}"
-      fridge_friends.each do |friend|
-        friend_phone = convert_phone_number(friend.phone_number)
-        @client.account.sms.messages.create(body: "#{current_user.name} just created a new item in #{current_fridge.name} called #{new_item.name}", to: friend_phone, from: '14153602257')
-        puts "Message sent to fridge friend: #{friend.name}"
-      end
-      redirect_to fridge_path(new_item.fridge.id)
+    @new_item = Item.new(params[:item])
+    @new_item.fridge_id = params[:fridge_id]
+    current_fridge = find_fridge(params[:fridge_id])
+    fridge_friends = find_only_friends_of_fridge(current_user, current_fridge)
+    if @new_item.save
+      text_current_user(twilio_client, current_user, current_fridge, @new_item)
+      text_fridge_friends(twilio_client, fridge_friends, current_user, current_fridge, @new_item)
+      redirect_to fridge_path(@new_item.fridge.id)
     else
-      flash[:add_item_notice] = new_item.errors.full_messages.join(", ")
-      redirect_to fridge_path(new_item.fridge.id)
+      flash[:add_item_notice] = @new_item.errors.full_messages.join(", ")
+      redirect_to fridge_path(@new_item.fridge.id)
     end
   end
 

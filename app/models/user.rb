3bfccef4a209f6b9af8713bb1ac09d05d6560caf
danbender <anniesing1014@gmail.com>
2013-11-09 18:21:07 -0800

    add phone number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   include Clearance::User
   attr_accessible :email, :password, :name, :phone_number, :customer_id
   validates :name, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: /\d{3}-\d{3}-\d{4}/
   has_and_belongs_to_many :fridges
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,7 @@ describe User do
   it { should validate_presence_of :phone_number }
   it { should validate_presence_of :name }
   it { should have_and_belong_to_many :fridges }
+  it { should allow_value('234-234-2342').for(:phone_number) }
+  it { should_not allow_value('2342342342').for(:phone_number) }
+  it { should_not allow_value('(234)234-2342').for(:phone_number) }
 end

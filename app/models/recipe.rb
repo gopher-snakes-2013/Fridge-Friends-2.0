956ec94c009828b0dc6b7432e8ad3655289e034e
carter <carter.sowers@gmail.com>
2013-11-11 11:20:33 -0800

    add model validations, db relationship test for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :name, :ingredients, :recipe_query_id
   belongs_to :recipe_query
+  validates :name, :ingredients, :recipe_query_id, presence: true
 end

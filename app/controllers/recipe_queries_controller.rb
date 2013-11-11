470e1a2a33a09cfa2e7dfb93d63e25331c0f9a2a
carter <carter.sowers@gmail.com>
2013-11-11 12:35:14 -0800

    add feature tests create and destroy recipe query

--- app/controllers/recipe_queries_controller.rb
+++ app/controllers/recipe_queries_controller.rb
@@ -8,10 +8,13 @@ class RecipeQueriesController < ApplicationController
   def create
     recipe_query = RecipeQuery.new(params[:recipe_query])
     recipe_query.user_id = current_user.id
-    recipe_query.save
-    result = Yummly.search(recipe_query.terms)
-    extract_recipes_from_search(recipe_query, result)
-    redirect_to recipe_query_path(recipe_query.id)
+    if recipe_query.save
+      result = Yummly.search(recipe_query.terms)
+      extract_recipes_from_search(recipe_query, result)
+      redirect_to recipe_query_path(recipe_query.id)
+    else
+      redirect_to recipe_queries_path
+    end
   end
 
   def show
@@ -20,6 +23,13 @@ class RecipeQueriesController < ApplicationController
   end
 
   def destroy
+    recipe_query = RecipeQuery.find(params[:id])
+    if current_user.id == recipe_query.user_id
+      recipe_query.destroy
+    else
+      flash[:delete_recipe_query_notice] = "You are not authorized to remove this recipe search"
+    end
+    redirect_to recipe_queries_path
   end
 
 end

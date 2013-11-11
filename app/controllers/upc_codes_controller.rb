7fb46940471c0f57d1267942d082b5468353b24b
Steven Nugent <stevennugentdev@gmail.com>
2013-11-10 23:09:09 -0800

    Image upload for UPC code working

--- app/controllers/upc_codes_controller.rb
+++ app/controllers/upc_codes_controller.rb
@@ -1,14 +1,12 @@
 class UpcCodesController < ApplicationController
   def create
-    p params
-    @upc_image = UpcCode.new(params[:upc_code])
-    if @upc_image.save
-      redirect_to @upc_image
-    else
-      p "EVERYTHING'S FUCKED!" * 50
-    end
+    @upc = UpcCode.new(params[:upc_code])
+    @upc.save
+    fridge_id = params[:fridge_id]
+    redirect_to fridge_upc_code_path(fridge_id, @upc.id)
   end
 
   def show
+    @upc = UpcCode.find(params[:id])
   end
 end

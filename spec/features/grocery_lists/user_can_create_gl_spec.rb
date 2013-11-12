d31d6d7df736b7efb7907e0ffe78c37d06e69e60
danbender <anniesing1014@gmail.com>
2013-11-12 15:34:29 -0800

    add gl feature test: fridge creator can add gl

--- spec/features/grocery_lists/user_can_create_gl_spec.rb
+++ spec/features/grocery_lists/user_can_create_gl_spec.rb
@@ -1,10 +1,37 @@
 require 'spec_helper'
 
-feature 'User may create Grocery List' do
-
-  scenario 'with valid params' do
-
+feature 'Fridge creator may create Grocery List' do
+  before(:each) do
+    visit root_path
   end
 
+  let!(:user) { User.new(name: 'Carter1', email: 'carter11@example.com', password: 'password', phone_number: '360-584-4437', customer_id: '123456788') }
 
+  scenario 'with valid params' do
+    signup_user(user)
+    fill_in "fridge_name", with: "Home Fridge"
+    click_on "Add Fridge"
+    click_on "Home Fridge"
+    fill_in "grocery_list_title", with: 'Grocery List'
+    click_on 'Create Grocery List for Fridge'
+    expect(page).to have_content 'Grocery List successfully created!'
+  end
+end
+
+feature 'Fridge creator may create Grocery List' do
+  before(:each) do
+    visit root_path
+  end
+
+  let!(:user) { User.new(name: 'Carter1', email: 'carter11@example.com', password: 'password', phone_number: '360-584-4437', customer_id: '123456788') }
+
+  scenario 'with valid params' do
+    signup_user(user)
+    fill_in "fridge_name", with: "Home Fridge"
+    click_on "Add Fridge"
+    click_on "Home Fridge"
+    fill_in "grocery_list_title", with: 'Grocery List'
+    click_on 'Create Grocery List for Fridge'
+    expect(page).to have_content 'Grocery List successfully created!'
+  end
 end

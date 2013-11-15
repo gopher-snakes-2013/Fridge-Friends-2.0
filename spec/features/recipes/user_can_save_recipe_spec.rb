4e687c858030156e80141fbacc0e78c0990cd60b
danbender <anniesing1014@gmail.com>
2013-11-14 16:33:56 -0800

    fix feature test: user can save recipe

--- spec/features/recipes/user_can_save_recipe_spec.rb
+++ spec/features/recipes/user_can_save_recipe_spec.rb
@@ -7,8 +7,11 @@ feature "User can save recipe" do
 
   let!(:user) { User.new(name: 'Carter1', email: 'carter11@example.com', password: 'password', phone_number: '360-584-4437', customer_id: '123456788') }
 
-  xscenario "user clicks on Add/Save Recipe" do
+  scenario "user clicks on Add/Save Recipe" do
     signup_user(user)
+    fill_in "fridge_name", with: "Test Fridge"
+    click_on "Add Fridge"
+    click_on "Test Fridge"
     visit recipe_queries_path
     fill_in "recipe_query_terms", with: "butternut"
     click_on "Find Recipes"
